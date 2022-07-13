bf07c591e580a490662a9578eee9abf55443134e
carki <94.nogari@gmail.com>
2022-07-14 01:45:01 +0900

    오토레이아웃으로 뷰 재배열 / ID,PW 입력을 안할 시 Placeholder Color -> Red로 변환되어 경고알림  /////// PW 6자리 미만 입력시 알림기능 + 추천인코드 숫자6자리 알림기능 추가 필요

--- Netflix Project/SignUpViewController.swift
+++ Netflix Project/SignUpViewController.swift
@@ -7,67 +7,62 @@
 
 import UIKit
 
-//텍스트필드 디자인(텍스트필드id, placeholder 텍스트)
-func textFieldDesign(textField: UITextField, placeholderContent: String) {
-    //placeholder + color
-    textField.attributedPlaceholder = NSAttributedString(string: placeholderContent, attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
-    textField.textColor = .white // 입력될 내용의 색상
-    textField.keyboardType = .default // 키보드 타입
-    textField.textAlignment = .center // 정렬
-    textField.borderStyle = .roundedRect // 모서리 스타일
-    textField.backgroundColor = .gray // 배경색
-    
+extension UITextField {
+    func setPlaceholder(color: UIColor) {
+        guard let string = self.placeholder else {
+            return
+        }
+        attributedPlaceholder = NSAttributedString(string: string, attributes: [.foregroundColor: color])
+    }
 }
-
-//버튼 디자인(버튼id, 버튼 텍스트)
-func btnDesign(btnId: UIButton, btnContent: String) {
-    btnId.setTitle(btnContent, for: .normal) //버튼 텍스트
-    btnId.setTitleColor(.black, for: .normal) // 버튼 텍스트 색
-    btnId.backgroundColor = .white // 버튼 배경 색
-    btnId.layer.cornerRadius = 10 // 모서리 둥글기 정도
-}
-    
-    
-//}
 
 class SignUpViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel! //제목 레이블
-    
-    //회원가입 항목 텍스트필드
-    @IBOutlet weak var idTextField: UITextField! //id
-    @IBOutlet weak var passwordTextField: UITextField! //pw
-    @IBOutlet weak var nicknameTextField: UITextField! //name
-    @IBOutlet weak var locationTextField: UITextField! //location
-    @IBOutlet weak var codeTextField: UITextField!  //code
-    
-    //회원가입 버튼
-    @IBOutlet weak var signupBtn: UIButton!
-    
-    //추가 정보 입력 레이블
-    @IBOutlet weak var inputLabel: UILabel!
-    
-    //스위치
-    @IBOutlet weak var switchUi: UISwitch!
+    @IBOutlet var textFieldCollection: [UITextField]!//userTextField Collection
+    @IBOutlet weak var signupBtn: UIButton! //회원가입 버튼
+    @IBOutlet weak var inputLabel: UILabel!//추가 정보 입력 레이블
+    @IBOutlet weak var switchUi: UISwitch!//스위치
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        titleLabel.textColor = .red
         inputLabel.textColor = .white
         
-        textFieldDesign(textField: idTextField, placeholderContent: "이메일 주소 또는 전화번호")
-        textFieldDesign(textField: passwordTextField, placeholderContent: "비밀번호")
-        textFieldDesign(textField: nicknameTextField, placeholderContent: "닉네임")
-        textFieldDesign(textField: locationTextField, placeholderContent: "위치")
-        textFieldDesign(textField: codeTextField, placeholderContent: "추천 코드 입력")
-        
+        textFieldDesign()
+        titleLabelDesign(label: titleLabel)
         btnDesign(btnId: signupBtn, btnContent: "회원가입")
         
         switchUi.onTintColor = .red
         switchUi.setOn(false, animated: true)
         switchUi.thumbTintColor = .white
+    }
+    
+    //텍스트필드 디자인(텍스트필드id, placeholder 텍스트)
+    func textFieldDesign() {
+        let place_holderArr = ["이메일 주소 또는 전화번호", "비밀번호", "닉네임", "위치", "추천 코드 입력"]
+        for i in 0..<textFieldCollection.count {
+            textFieldCollection[i].backgroundColor = .gray
+            textFieldCollection[i].placeholder = place_holderArr[i]
+            textFieldCollection[i].setPlaceholder(color: .white)
+            textFieldCollection[i].textAlignment = .center
+        }
+    }
+    
+    func titleLabelDesign(label: UILabel) {
+        label.text = "NFLIX"
+        label.textAlignment = .center
+        label.textColor = .red
+        label.font = .boldSystemFont(ofSize: 50)
+    }
+
+    //버튼 디자인(버튼id, 버튼 텍스트)
+    func btnDesign(btnId: UIButton, btnContent: String) {
+        btnId.setTitle(btnContent, for: .normal) //버튼 텍스트
+        btnId.setTitleColor(.black, for: .normal) // 버튼 텍스트 색
+        btnId.backgroundColor = .white // 버튼 배경 색
+        btnId.layer.cornerRadius = 5 // 모서리 둥글기 정도
     }
     
     //회원가입 버튼 클릭
@@ -79,5 +74,30 @@ class SignUpViewController: UIViewController {
     @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
+    
+    
+    @IBAction func idTextFieldClicked(_ sender: UITextField) {
+        textFieldCollection[0].placeholder = ""
+        
+        
+        if textFieldCollection[0].text == ""  && textFieldCollection[0].placeholder == "" {
+            textFieldCollection[0].placeholder = "ID는 필수로 입력해주셔야 합니다."
+            textFieldCollection[0].setPlaceholder(color: .red)
+        }
+    }
+    
+    @IBAction func passwordTextFieldClicked(_ sender: UITextField) {
+        textFieldCollection[1].placeholder = ""
+        
+        if textFieldCollection[1].text == ""  && textFieldCollection[1].placeholder == "" {
+            textFieldCollection[1].placeholder = "PW는 필수로 입력해주셔야 합니다."
+            textFieldCollection[1].setPlaceholder(color: .red)
+            
+//            if textFieldCollection[1].text.count < 6 {
+//                textFieldCollection[1].placeholder = "PW는 최소 6자 이상으로 입력해주셔야 합니다."
+//            }
+        }
+    }
+    
     
 }
